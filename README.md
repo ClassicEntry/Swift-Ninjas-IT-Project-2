4e28cdd95a7de58254fd342b5e7305fd6a718ace
Youssef Abdelhamid <youssef.hamdy0207@gmail.com>
2024-08-07 03:02:26 +1000

    tabs

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ Run the following the terminal as root
 3. `cd into directroy of the project`
 4. `npm start`
 
+npm install @react-native-async-storage/async-storage
+npm install @react-navigation/native @react-navigation/bottom-tabs
+npm install react-native-screens react-native-safe-area-context
+
 ## HOW TO INSTALL FOR MAC
---
+++ event-scheduler-app/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TaskManagerScreen from './TaskManagerScreen';
+import AnotherScreen from './AnotherScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Tasks" component={TaskManagerScreen} />
+                <Tab.Screen name="Another" component={AnotherScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
